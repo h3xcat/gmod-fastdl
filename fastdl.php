1813ac295a350afe68aae7514c6a024cb2cb0612
edgarasf123 <edgarasf123@gmail.com>
2016-07-03 16:54:03 -0700

    Use realpaths to check for traversal attacks.

--- fastdl.php
+++ fastdl.php
@@ -2,7 +2,7 @@
 define("GAME_DIR", "/home/steam/gmod_server/garrysmod");
 define("CACHE_DIR", "./fastdl_cache");
 define("SCAN_ADDONS", true);
-define("ATTEMPTS_LOG_FILE", "");
+define("ATTEMPTS_LOG_FILE", "./attempts.log");
 
 $resource_dirs = [
 	"maps",
@@ -28,52 +28,62 @@ $alllowed_extensions = [
 	"bsp"
 ];
 
-$file = $_GET["f"];
-if( substr($file, -4) != ".bz2" ){
-	header("HTTP/1.0 404 Not Found");
-	die('404 Not Found');
-}
-$file = substr($file, 0, -4);;
-$file_info = pathinfo($file);
 
-//Valide the request
-if(
-	empty($file) ||
-	strpos($file,"..") ||
-	!in_array($file_info["extension"], $alllowed_extensions) ||
-	!in_array(explode("/",$file)[0], $resource_dirs)
-){
+function attempt_log($str){
 	if(!empty(ATTEMPTS_LOG_FILE)){
-		file_put_contents(ATTEMPTS_LOG_FILE, date('d.m.Y H:i:s')." - ".$_SERVER['REMOTE_ADDR']." ".$file."\r\n",FILE_APPEND);
+		file_put_contents(ATTEMPTS_LOG_FILE, date('d.m.Y H:i:s')." - ".$str."\r\n",FILE_APPEND);
 	}
+}
+
+function exit404(){
 	header("HTTP/1.0 404 Not Found");
 	die('404 Not Found');
 }
-//Redirect if cached file already exist
-if(file_exists(CACHE_DIR."/".$file.".bz2")){
-	header('Location: ' .CACHE_DIR."/".$file.".bz2" , true, 302);
-	exit();
-}
 
-//If file doesn't exist, try find it and cache it.
-$scan_dirs = [];
-if(SCAN_ADDONS){
-	$scan_dirs = glob(GAME_DIR.'/addons/*' , GLOB_ONLYDIR);
-}
-array_unshift($scan_dirs,GAME_DIR);
-foreach($scan_dirs as $dir){
-	if(file_exists($dir."/".$file)){
-		mkdir(CACHE_DIR."/".$file_info["dirname"], 0755, true);
-		$data = file_get_contents($dir."/".$file);
-		$bz = bzopen(CACHE_DIR."/".$file.".bz2", "w");
+
+$file = $_SERVER["QUERY_STRING"];
+if( substr($file, -4) != ".bz2" ){ exit404(); }
+$file = substr($file, 0, -4);;
+
+$file_info = pathinfo($file);
+$cfile_path = CACHE_DIR."/".$file.".bz2";
+$cfile_rpath = realpath($cfile_path);
+$cache_rpath = realpath(CACHE_DIR);
+
+if($cfile_rpath === false){ // Cached file doesn't exist
+	$scan_dirs = [];
+	if(SCAN_ADDONS){
+		$scan_dirs = glob(GAME_DIR.'/addons/*' , GLOB_ONLYDIR);
+	}
+	array_unshift($scan_dirs,GAME_DIR);
+
+	$created = false;
+	foreach($scan_dirs as $dir){
+		$dir_rpath = realpath($dir);
+		$file_rpath = realpath($dir_rpath."/".$file);
+		if($file_rpath === false){
+			continue;
+		}elseif(strpos($file_rpath, $dir_rpath."/") !== 0) {
+			attempt_log($_SERVER['REMOTE_ADDR']." ".$_SERVER['REQUEST_URI']);
+			exit404();
+		}
+		// Create cached file
+		mkdir($cache_rpath."/".$file_info["dirname"], 0755, true);
+
+		$data = file_get_contents($file_rpath);
+		$bz = bzopen($cfile_path, "w");
 		bzwrite($bz, $data, strlen($data));
 		bzclose($bz);
-
-		header('Location: ' .CACHE_DIR."/".$file.".bz2" , true, 302);
-		exit();
+		$created = true;
 	}
+	if(!$created){
+		exit404();
+	}
+}elseif(strpos($cfile_rpath, $cache_rpath."/") !== 0) { // Cached file exists, but not where it should be. Possible traversal attack?
+	attempt_log($_SERVER['REMOTE_ADDR']." ".$_SERVER['REQUEST_URI']);
+	exit404();
 }
 
-header("HTTP/1.0 404 Not Found");
-die('404 Not Found'.$file);
+header('Location: ' . $cfile_path , true, 302);
+exit();
 ?>
