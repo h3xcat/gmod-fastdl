f1218885ecd081d28566757d43efe4bc571cfcdc
edgarasf123 <edgarasf123@gmail.com>
2016-07-04 00:12:35 -0700

    Fix directory creation

--- fastdl.php
+++ fastdl.php
@@ -5,7 +5,6 @@ define("SCAN_ADDONS", true);
 define("SCAN_GAMEMODES", true);
 define("ATTEMPTS_LOG_FILE", "./attempts.log");
 
-
 $content_dirs = [
 	"maps",
 	"resource",
@@ -13,7 +12,6 @@ $content_dirs = [
 	"sound",
 	"models"
 ];
-
 $alllowed_extensions = [
 	"wav",
 	"mp3",
@@ -29,28 +27,21 @@ $alllowed_extensions = [
 	"ain",
 	"bsp"
 ];
-
-
 function attempt_log($str){
 	if(!empty(ATTEMPTS_LOG_FILE)){
 		file_put_contents(ATTEMPTS_LOG_FILE, date('d.m.Y H:i:s')." - ".$str."\r\n",FILE_APPEND);
 	}
 }
-
 function exit404(){
 	header("HTTP/1.0 404 Not Found");
 	die('404 Not Found');
 }
-
-
 $file = $_SERVER["QUERY_STRING"];
 if( substr($file, -4) != ".bz2" ){ exit404(); }
 $file = substr($file, 0, -4);;
-
 $cfile_path = CACHE_DIR.$file.".bz2";
 $cfile_rpath = realpath($cfile_path);
 $cache_rpath = realpath(CACHE_DIR);
-
 if($cfile_rpath === false){ // Cached file doesn't exist
 	$scan_dirs = [GAME_DIR];
 	if(SCAN_ADDONS){
@@ -59,13 +50,12 @@ if($cfile_rpath === false){ // Cached file doesn't exist
 	if(SCAN_GAMEMODES){
 		$scan_dirs = array_merge($scan_dirs,glob(GAME_DIR.'/gamemodes/*/content' , GLOB_ONLYDIR));
 	}
-
 	$created = false;
 	foreach($scan_dirs as $dir){
 		$dir_rpath = realpath($dir);
 		$file_rpath = realpath($dir_rpath.$file);
 		$file_info = pathinfo($file_rpath);
-
+		$cfile_info = pathinfo($cfile_path);
 		if($file_rpath === false){
 			continue;
 		}elseif(strpos($file_rpath, $dir_rpath."/") !== 0) {
@@ -78,9 +68,7 @@ if($cfile_rpath === false){ // Cached file doesn't exist
 			attempt_log("Invalid content directory: ".$_SERVER['REMOTE_ADDR']." ".$_SERVER['REQUEST_URI']);
 			exit404();
 		}
-
-		mkdir($file_info["dirname"], 0755, true);
-
+		mkdir($cfile_info["dirname"], 0755, true);
 		$data = file_get_contents($file_rpath);
 		$bz = bzopen($cfile_path, "w");
 		bzwrite($bz, $data, strlen($data));
@@ -94,7 +82,6 @@ if($cfile_rpath === false){ // Cached file doesn't exist
 	attempt_log("Invalid cache file location: ".$_SERVER['REMOTE_ADDR']." ".$_SERVER['REQUEST_URI']);
 	exit404();
 }
-
 header('Location: ' . $cfile_path , true, 302);
 exit();
 ?>
